5dcdb9637fda2682b0baa1971d1f9d7f77ab5a90
Ivan <nerounxqxq@gmail.com>
2025-05-24 16:13:58 +0400

    Добавлены константы

--- TickTackToes/Field.cs
+++ TickTackToes/Field.cs
@@ -2,6 +2,9 @@
 {
     public class Field
     {
+        private const int FIRST_CELL_X_POSITION = 1;
+        private const int FIRST_CELL_Y_POSITION = 1;
+        private const FieldFillType EMPTY_CELL = FieldFillType.EMPTY;
         public FieldFillType[,] field { get; }
 
         public int fieldSize { get; }
@@ -11,30 +14,29 @@
             field = new FieldFillType[fieldSize, fieldSize];
         }
 
-
         public void MakeMove(int x, int y, FieldFillType playerType, ref bool isWin, ref bool isSuccessMove)
         {
             if (CheckFillOfCell(x, y))
             {
-                field[x-1, y-1] = playerType;
-                if(!CheckForWin(x, y).Equals(FieldFillType.EMPTY))
+                field[x - 1, y - 1] = playerType;
+                if (!CheckForWin(x, y).Equals(EMPTY_CELL))
                 {
                     isWin = true;
                 }
                 isSuccessMove = true;
             }
             else
-                isSuccessMove= false;
+                isSuccessMove = false;
         }
 
         private bool CheckFillOfCell(int x, int y)
         {
-            if (x < 1 || x > field.GetLength(0) || y < 1 || y > field.GetLength(1))
+            if (x < FIRST_CELL_X_POSITION || x > field.GetLength(0) || y < FIRST_CELL_Y_POSITION || y > field.GetLength(1))
             {
                 Console.WriteLine("Клетка находится за пределами поля");
                 return false;
             }
-            if (!field[x-1, y-1].Equals(FieldFillType.EMPTY))
+            if (!field[x - 1, y - 1].Equals(EMPTY_CELL))
             {
                 Console.WriteLine("Эта клетка уже заполнена! Введите другую");
                 return false;
@@ -46,7 +48,7 @@
         {
             for (int i = 0; i < fieldSize; i++)
             {
-                if (CheckLine(0, i, 1, 0)) 
+                if (CheckLine(0, i, 1, 0))
                     return field[i, 0];
             }
 
@@ -68,7 +70,7 @@
         private bool CheckLine(int startX, int startY, int dx, int dy)
         {
             FieldFillType symbol = field[startY, startX];
-            if (symbol is FieldFillType.EMPTY)
+            if (symbol is EMPTY_CELL)
                 return false;
 
             for (int i = 0; i < fieldSize; i++)
--- TickTackToes/Game.cs
+++ TickTackToes/Game.cs
@@ -8,9 +8,15 @@ namespace TickTackToes
 {
     public class Game
     {
-        private Field field { get; }
+        private const FieldFillType NOUGHT = FieldFillType.NOUGHT;
 
-        private const string menu = "1)Играть\n2)Выход";
+        private const FieldFillType CROSS = FieldFillType.CROSS;
+
+        private const string MENU = "1)Играть\n2)Выход";
+
+        private const string TABLE_HEADER = " ------------ ";
+
+        private Field field { get; }
 
         public Game(int fieldSize)
         {
@@ -22,7 +28,7 @@ namespace TickTackToes
             int movesCount = 0;
             while (true)
             {
-                Console.WriteLine(menu);
+                Console.WriteLine(MENU);
                 int choise = 0;
 
                 if (int.TryParse(Console.ReadLine(), out choise))
@@ -40,10 +46,10 @@ namespace TickTackToes
                                     break;
                                 PrintGameField();
                                 bool isMoveSuccess = false;
-                                int[] coordinates = InputMove(movesCount % 2 == 0 ? FieldFillType.CROSS : FieldFillType.NOUGHT);
-                                if(coordinates!=null && coordinates.Length == 2)
+                                int[] coordinates = InputMove(movesCount % 2 == 0 ? CROSS : NOUGHT);
+                                if (coordinates != null && coordinates.Length == 2)
                                 {
-                                    player = movesCount % 2 == 0 ? FieldFillType.CROSS : FieldFillType.NOUGHT;
+                                    player = movesCount % 2 == 0 ? CROSS : NOUGHT;
                                     field.MakeMove(coordinates[0], coordinates[1], player, ref isWin, ref isMoveSuccess);
                                     if (isMoveSuccess)
                                         movesCount++;
@@ -53,7 +59,7 @@ namespace TickTackToes
                                 Console.Clear();
                             }
                             PrintGameField();
-                            Console.WriteLine((movesCount == cellsNeedToFill && !isWin) ? "Ничья!" : ("Игра окончена! Победитель: " + (player.Equals(FieldFillType.CROSS) ? "крестики" : "нолики")));
+                            Console.WriteLine((movesCount == cellsNeedToFill && !isWin) ? "Ничья!" : ("Игра окончена! Победитель: " + (player.Equals(CROSS) ? "крестики" : "нолики")));
 
                             break;
                         case 2:
@@ -75,12 +81,12 @@ namespace TickTackToes
         private int[] InputMove(FieldFillType player)
         {
             int[] coordinates = null;
-            Console.WriteLine($"Сейчас ходят {(player.Equals(FieldFillType.CROSS) ? "крестики" : "нолики")}");
+            Console.WriteLine($"Сейчас ходят {(player.Equals(CROSS) ? "крестики" : "нолики")}");
             Console.Write("Введите через пробел координаты (x,y), где (1,1) - левый верхний угол: ");
             try
             {
                 coordinates = Console.ReadLine().Split().Select(int.Parse).ToArray();
-                if(coordinates.Length != 2)
+                if (coordinates.Length != 2)
                 {
                     throw new Exception("Нужно ввести 2 координаты!");
                 }
@@ -102,17 +108,17 @@ namespace TickTackToes
 
                     if (i == 0 && j == 0)
                     {
-                        Console.WriteLine(" ------------ ");
+                        Console.WriteLine(TABLE_HEADER);
                     }
                     if (j == 0)
                     {
-                        
+
                         Console.Write("| ");
                     }
                     var fieldType = field.field[i, j];
-                    Console.Write(fieldType == FieldFillType.CROSS ? " X |" : (fieldType == FieldFillType.NOUGHT ? " O |" : " * |"));
+                    Console.Write(fieldType == CROSS ? " X |" : (fieldType == NOUGHT ? " O |" : " * |"));
                 }
-                Console.WriteLine(i == field.fieldSize - 1 ? "\n ------------ " : "\n");
+                Console.WriteLine(i == field.fieldSize - 1 ? "\n" + TABLE_HEADER : "\n");
             }
 
         }
